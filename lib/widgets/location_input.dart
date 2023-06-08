f2280924007a0384c9d5be33f09fdf4c992bc571
ivan kopech <ivankopech@gmail.com>
2023-06-08 15:05:09 -0300

    widget de ubicacion

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.location_on),
+              label: const Text('Get current location'),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.map),
+              label: const Text('Select location on map'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
