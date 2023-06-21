39dde7ce049eeb55dd955e7b0fc293d0d7e67394
ivan kopech <ivankopech@gmail.com>
2023-06-21 16:00:47 -0300

    apretar ubicacion elegida lleva a mapa

--- lib/screen/places_detail.dart
+++ lib/screen/places_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../model/place.dart';
+import './map.dart';
 
 class PlaceDetailScreen extends StatelessWidget {
   const PlaceDetailScreen({super.key, required this.place});
@@ -33,9 +34,21 @@ class PlaceDetailScreen extends StatelessWidget {
             right: 0,
             child: Column(
               children: [
-                CircleAvatar(
-                  radius: 70,
-                  backgroundImage: NetworkImage(locationImage),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (ctx) => MapScreen(
+                          location: place.location,
+                          isSelecting: false,
+                        ),
+                      ),
+                    );
+                  },
+                  child: CircleAvatar(
+                    radius: 70,
+                    backgroundImage: NetworkImage(locationImage),
+                  ),
                 ),
                 Container(
                   alignment: Alignment.center,
